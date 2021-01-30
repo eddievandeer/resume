1e8c9f8dbafe7c4d631f3da8b50fcf5be8c455af
eddievandeer <1255173159@qq.com>
2021-01-30 15:03:26 +0800

    feat(global): register global components

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
+import MyUI from './components/my-ui'
 
 const app = createApp(App)
 app.use(store)
+app.use(MyUI)
 app.mount('#app')
