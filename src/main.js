e9fafaeb8be6d0bbaef64af2847ae87b56719798
eddievandeer <1255173159@qq.com>
2021-02-01 12:23:38 +0800

    feat: use global style and import font-awesome

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
 import MyUI from './components/my-ui'
+import './style/global.scss'
+import 'font-awesome/css/font-awesome.min.css'
 
 const app = createApp(App)
 app.use(store)
