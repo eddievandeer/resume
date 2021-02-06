654ece057ba92d7dafd81b6cd735cc8b34257766
eddievandeer <1255173159@qq.com>
2021-02-06 21:43:11 +0800

    feat(print-btn): tuen style to class name control

--- src/components/PrintBtn.vue
+++ src/components/PrintBtn.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="print-wrapper">
-        <button class="print-btn" @click="ExportSavePdf('#pdfCentent',title)">
+        <button class="print-btn btn normal" @click="ExportSavePdf('#pdfCentent',title)">
             <i class="fa fa-print" aria-hidden="true"></i>
             <span>打印简历</span>
         </button>
@@ -31,17 +31,6 @@
         right: 1rem;
 
         .print-btn {
-            color: #606266;
-            border-radius: 4px;
-            margin: 0;
-            padding: 12px 20px;
-            background: #fff;
-            border: 1px solid #dcdfe6;
-            box-sizing: border-box;
-            transition: .1s;
-            -moz-user-select: none;
-            -webkit-user-select: none;
-            -ms-user-select: none;
 
             i {
                 font-size: 24px;
@@ -50,17 +39,6 @@
             span {
                 font-size: 14px;
                 margin-left: .5rem;
-            }
-
-            &:hover {
-                color: #409eff;
-                border-color: #c6e2ff;
-                background-color: #ecf5ff;
-            }
-
-            &:active {
-                color: #3a8ee6;
-                border-color: #3a8ee6;
             }
         }
     }
