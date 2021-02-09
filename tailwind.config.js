de8542eaf3303a70148f500e91052d20a9327d13
eddievandeer <1255173159@qq.com>
2021-02-09 16:46:28 +0800

    chore: add configuration file of tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./**/*.vue'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
