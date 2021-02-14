2aca1fa3e6bff3d4c8eae0e63d25f12ad541ca9c
eddievandeer <1255173159@qq.com>
2021-02-14 20:51:22 +0800

    feat(skills): change the template structure

--- src/components/my-ui/MySkills/SkillItem.vue
+++ src/components/my-ui/MySkills/SkillItem.vue
@@ -1,8 +1,15 @@
 <template>
-    <div class="list-item skill" v-for="skill in item.skill" :key="skill">
-        <h4 class="item-title">{{ skill.title }}</h4>
-        <div class="item-wrapper" v-for="description in skill.descriptions" :key="description">
-            <p class="description"><i class="fa fa-circle-o" aria-hidden="true"></i>{{description}}</p>
+    <div class="list-item">
+        <div class="skill" v-for="skill in item.skill" :key="skill">
+            <h4 class="item-title">{{ skill.title }}</h4>
+            <div class="item-wrapper" v-for="description in skill.descriptions" :key="description">
+                <p class="description"><i class="fa fa-circle-o" aria-hidden="true"></i>{{description}}</p>
+            </div>
+        </div>
+        <div class="else">
+            <div class="item-wrapper" v-for="description in item.descriptions" :key="description">
+                <p class="description"><i class="fa fa-circle-o" aria-hidden="true"></i>{{description}}</p>
+            </div>
         </div>
     </div>
 </template>
