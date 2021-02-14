830afc5fdb7303de106111fb554063342cb39fc4
eddievandeer <1255173159@qq.com>
2021-02-14 19:14:32 +0800

    feat(info): add class for h2 tag

--- src/components/my-ui/PersonalInfo/InfoDetail.vue
+++ src/components/my-ui/PersonalInfo/InfoDetail.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="info-detail">
         <div class="info-detail-item introduction">
-            <h2><i class="fa fa-address-card-o" aria-hidden="true"></i>个人介绍</h2>
+            <h2 class="title"><i class="fa fa-address-card-o" aria-hidden="true"></i>个人介绍</h2>
             <p>性别：{{info.gender}}</p>
             <p>毕业院校：{{info.education.university}}</p>
             <p>专业：{{info.education.major}}</p>
         </div>
         <div class="info-detail-item contact">
-            <h2><i class="fa fa-envelope-o" aria-hidden="true"></i>联系方式</h2>
+            <h2 class="title"><i class="fa fa-envelope-o" aria-hidden="true"></i>联系方式</h2>
             <p>邮箱：{{info.contact.email}}</p>
             <p>电话：{{info.contact.tel}}</p>
         </div>
         <div class="info-detail-item else">
-            <h2><i class="fa fa-at" aria-hidden="true"></i>其他</h2>
+            <h2 class="title"><i class="fa fa-at" aria-hidden="true"></i>其他</h2>
             <p>GitHub：<a :href="info.github" target="__blank">{{info.github}}</a></p>
             <p>博客网站：<a :href="info.blog" target="__blank">{{info.blog}}</a></p>
         </div>
@@ -32,23 +32,24 @@
 <style lang="scss" scoped>
     .info-detail {
         width: 100%;
-        margin-top: 1rem;
+        margin-top: 1.6rem;
 
         .info-detail-item {
-            margin-bottom: 1rem;
-        }
+            margin-bottom: 1.2rem;
 
-        h2 {
-            letter-spacing: 1px;
+            h2 {
+                letter-spacing: 1px;
+                margin-bottom: .6rem;
 
-            i {
-                margin-right: .8rem;
+                i {
+                    margin-right: .8rem;
+                }
             }
-        }
 
-        a,
-        p {
-            color: white;
+            a,
+            p {
+                color: white;
+            }
         }
     }
 
