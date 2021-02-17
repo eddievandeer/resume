bb68eecd87f664d4c0490c588c7b03902d3ff7cb
eddievandeer <1255173159@qq.com>
2021-02-17 21:50:03 +0800

    feat(my-ui): add experience type and change the h3 tag to h4

--- src/components/my-ui/MyExperiences/ExperienceItem.vue
+++ src/components/my-ui/MyExperiences/ExperienceItem.vue
@@ -1,6 +1,7 @@
 <template>
+    <h3>{{item.type}}</h3>
     <div class="list-item experience" v-for="experience in item.details" :key="experience">
-        <h3 class="item-title">{{ experience.title }}</h3>
+        <h4 class="item-title">{{ experience.title }}</h4>
         <div class="item-wrapper" v-for="description in experience.descriptions" :key="description">
             <p class="description"><i class="fa fa-circle-o" aria-hidden="true"></i>{{description}}</p>
         </div>
