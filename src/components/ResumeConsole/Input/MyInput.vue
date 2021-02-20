a9dbad60e3d3e14e07b8dadfe8235e708e44774a
eddievandeer <1255173159@qq.com>
2021-02-20 16:20:27 +0800

    feat(console): reseal input box component and add clear function

--- src/components/ResumeConsole/Input/MyInput.vue
+++ src/components/ResumeConsole/Input/MyInput.vue
@@ -1,63 +1,71 @@
 <template>
     <div class="console-input-wrapper">
-        <label for="console-input" class="console-input-label" :style="styleObject">
+        <label class="console-input-label" :style="styleObject">
             <slot></slot>
         </label>
-        <input id="console-input" type="text" class="console-input" v-model="info[type]" @focus="inFocus"
-            @blur="inBlur">
+        <button class="close-btn" @click="handleClear"><i class="fa fa-times-circle-o" aria-hidden="true"></i></button>
+        <input type="text" class="console-input" :value="value" @input="handleInput" @focus="handleFocus"
+            @blur="handleBlur">
     </div>
 </template>
 
 <script>
     import {
-        reactive
+        reactive,
+        watch
     } from 'vue'
-
-    import {
-        useStore
-    } from 'vuex'
 
     export default {
         name: 'MyInput',
         props: {
-            type: String
+            value: String
         },
-        setup(props) {
-            const store = useStore()
-
-            const info = store.state.personalInfo
-
+        setup(props, context) {
             const styleObject = reactive({
                 top: '0',
                 fontSize: '14px',
                 color: '#a6a9b1'
             })
 
-            if (!haveContent(info[props.type])) {
-                styleObject.top = '30px'
-                styleObject.fontSize = '16px'
+            if (!haveContent(props.value)) {
+                toBottom(styleObject)
             }
 
-            function inFocus() {
-                styleObject.top = '0'
-                styleObject.fontSize = '14px'
+            watch(() => props.value, () => {
+                if (haveContent(props.value)) {
+                    toTop(styleObject)
+                } else {
+                    toBottom(styleObject)
+                }
+            })
+
+            function handleInput(e) {
+                context.emit('handleInput', e.target.value)
+            }
+
+            function handleFocus() {
+                toTop(styleObject)
                 styleObject.color = '#409eff'
             }
 
-            function inBlur() {
+            function handleBlur() {
                 styleObject.color = '#a6a9b1'
-                if (haveContent(info[props.type])) {
+                if (haveContent(props.value)) {
                     return
                 }
-                styleObject.top = '30px'
-                styleObject.fontSize = '16px'
+                toBottom(styleObject)
+            }
+
+            function handleClear() {
+                context.emit('handleInput', '')
             }
 
             return {
-                info,
                 styleObject,
-                inFocus,
-                inBlur
+                handleInput,
+                handleFocus,
+                handleBlur,
+                handleClear
             }
         }
     }
@@ -66,21 +74,44 @@
         return target && target.length > 0
     }
 
+    function toBottom(styleObject) {
+        styleObject.top = '30px'
+        styleObject.fontSize = '16px'
+    }
+
+    function toTop(styleObject) {
+        styleObject.top = '0'
+        styleObject.fontSize = '14px'
+    }
+
 </script>
 
 <style lang="scss">
     .console-input-wrapper {
-        height: 3.5rem;
-        margin-bottom: 1rem;
+        height: 3.4rem;
+        margin-bottom: .8rem;
         display: flex;
         flex-direction: column;
         justify-content: flex-end;
         position: relative;
 
+        .close-btn {
+            color: #c0c4cc;
+            background-color: transparent;
+            position: absolute;
+            bottom: .7rem;
+            right: .3rem;
+
+            &:hover {
+                color: #909399;
+            }
+        }
+
         .console-input-label {
             width: fit-content;
             font-size: 14px;
             transition: all .2s ease-in-out;
+            pointer-events: none;
             position: absolute;
 
             &:focus {
