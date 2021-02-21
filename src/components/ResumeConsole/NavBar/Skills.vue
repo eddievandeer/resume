2666448436547e5ed78f6f89b1080b0054144106
eddievandeer <1255173159@qq.com>
2021-02-21 16:59:40 +0800

    feat(console): set the content of the skill section

--- src/components/ResumeConsole/NavBar/Skills.vue
+++ src/components/ResumeConsole/NavBar/Skills.vue
@@ -1,9 +1,16 @@
 <template>
     <div class="inputs">
-        <div class="input-item" v-for="(type, key) in skillItem" :key="key">
-            <my-input :type="key">
-                <span>{{type}}</span>
-            </my-input>
+        <div class="input-item" v-for="(item, key) in skills" :key="key">
+            <drawer :title="item.type">
+                <my-input v-model:value="item.type">
+                    <span>{{skillItem.type}}</span>
+                </my-input>
+                <div class="input-item" v-for="(skill, key) in item.skill" :key="key">
+                    <my-input v-model:value="skill.title">
+                        <span>{{skillItem.skill[0].title}}</span>
+                    </my-input>
+                </div>
+            </drawer>
         </div>
     </div>
 </template>
@@ -12,26 +19,36 @@
     import {
         reactive
     } from 'vue'
+
+    import {
+        useStore
+    } from 'vuex'
+
     import MyInput from '../Input/MyInput'
+    import Drawer from '../Input/Drawer.vue'
 
     export default {
         components: {
-            MyInput
+            MyInput,
+            Drawer
         },
         setup() {
+            const store = useStore()
+
+            const skills = store.state.skills
+
             const skillItem = reactive({
-                name: '姓名',
-                job: '求职岗位',
-                gender: '输入性别',
-                university: '毕业院校',
-                major: '所学专业',
-                email: '联系邮箱',
-                tel: '联系电话',
-                github: 'GitHub主页',
-                blog: '博客网站'
+                type: '类型',
+                skill: [{
+                    title: '标题',
+                    descriptions: [
+                        '描述'
+                    ]
+                }]
             })
 
             return {
+                skills,
                 skillItem
             }
         }
