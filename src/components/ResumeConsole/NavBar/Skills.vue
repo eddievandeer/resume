7951dc7dbbc5b8d6370d107a072cd5d313217190
eddievandeer <1255173159@qq.com>
2021-02-24 19:47:57 +0800

    feat(console): bind input box to data in skill section

--- src/components/ResumeConsole/NavBar/Skills.vue
+++ src/components/ResumeConsole/NavBar/Skills.vue
@@ -1,14 +1,22 @@
 <template>
     <div class="inputs">
-        <div class="input-item" v-for="(item, key) in skills" :key="key">
+        <div v-for="(item, key1) in skills" :key="key1">
             <drawer :title="item.type">
-                <my-input v-model:value="item.type">
+                <my-input v-model:value="item.type" @handleInput="handleAttribute(item, 'type', $event)">
                     <span>{{skillItem.type}}</span>
                 </my-input>
-                <div class="input-item" v-for="(skill, key) in item.skill" :key="key">
-                    <my-input v-model:value="skill.title">
+                <div class="input-item" v-for="(skill, key2) in item.skill" :key="key2">
+                    <my-input v-model:value="skill.title" @handleInput="handleAttribute(skill, 'title', $event)">
                         <span>{{skillItem.skill[0].title}}</span>
                     </my-input>
+                    <div class="input-item">
+                        <div v-for="(description, index) in skill.descriptions" :key="index">
+                            <my-input :value="description" @handleInput="handleArray(skill.descriptions, index, $event)"
+                                type="textarea">
+                                <span>{{'描述'+(index+1)}}</span>
+                            </my-input>
+                        </div>
+                    </div>
                 </div>
             </drawer>
         </div>
@@ -49,14 +57,33 @@
 
             return {
                 skills,
-                skillItem
+                skillItem,
+                handleAttribute,
+                handleArray
             }
         }
+    }
+
+    function handleAttribute(target, key, value) {
+        target[key] = value
+    }
+
+    function handleArray(target, index, value) {
+        target[index] = value
     }
 
 </script>
 
 <style lang="scss" scoped>
+    .input-item {
+        border: 1px solid #ebebeb;
+        padding: .5rem 1rem;
+        margin-bottom: .6rem;
+        border-radius: 5px;
 
+        &:hover {
+            box-shadow: 0 0 8px 0 rgb(232 237 250 / 60%), 0 2px 4px 0 rgb(232 237 250 / 50%);
+        }
+    }
 
 </style>
