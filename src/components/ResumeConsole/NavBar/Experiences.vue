608e073e548e3f29e0488d8a15c721a4e468b488
eddievandeer <1255173159@qq.com>
2021-02-26 10:59:04 +0800

    feat(console): set the content of the experience section

--- src/components/ResumeConsole/NavBar/Experiences.vue
+++ src/components/ResumeConsole/NavBar/Experiences.vue
@@ -1,10 +1,53 @@
 <template>
-    Experiences
+    <div class="inputs">
+        <div v-for="(item, key) in experiences" :key="key">
+            <drawer :title="item.type">
+                <my-input v-model="item.type">
+                    <span>类型</span>
+                </my-input>
+                <div class="input-item" v-for="(detail, index1) in item.details" :key="index1">
+                    <my-input v-model="detail.title">
+                        <span>标题</span>
+                    </my-input>
+                    <div class="input-item">
+                        <div v-for="(description, index2) in detail.descriptions" :key="index2">
+                            <my-input v-model="detail.descriptions[index2]" type="textarea">
+                                <span>{{'描述'+(index2+1)}}</span>
+                            </my-input>
+                        </div>
+                    </div>
+                </div>
+            </drawer>
+        </div>
+    </div>
 </template>
 
 <script>
-    export default {
+    import {
+        reactive
+    } from 'vue'
 
+    import {
+        useStore
+    } from 'vuex'
+
+    import MyInput from '../Input/MyInput'
+    import Drawer from '../Input/Drawer.vue'
+
+    export default {
+        components: {
+            MyInput,
+            Drawer
+        },
+        setup() {
+            const store = useStore()
+
+            const experiences = store.state.experiences
+
+            return {
+                experiences
+            }
+        }
     }
 
 </script>
