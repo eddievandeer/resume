664cfd318957fdb629c4d2f012b9522b3f21e67e
eddievandeer <1255173159@qq.com>
2021-03-04 22:11:52 +0800

    chore(store): remove redundant mutations and add comments

--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -1,25 +1,34 @@
+// 设置个人信息
 export const SET_PERSONAL_INFO = 'SET_PERSONAL_INFO'
 
+// 设置技能信息
 export const SET_SKILLS = 'SET_SKILLS'
 
+// 设置经历信息
 export const SET_EXPERIENCES = 'SET_EXPERIENCES'
 
+// 设置模块信息（未实现）
 export const SET_PARTS = 'SET_PARTS'
 
+// 添加含type的技能（大类）
 export const ADD_SKILL_ITEM = 'ADD_SKILL_ITEM'
 
-export const SET_SKILL_ITEM_TYPE = 'SET_SKILL_ITEM_TYPE'
+// export const SET_SKILL_ITEM_TYPE = 'SET_SKILL_ITEM_TYPE'
 
-export const ADD_SKILL = 'ADD_SKILL'
+// export const ADD_SKILL = 'ADD_SKILL'
 
-export const SET_SKILL_TITLE = 'SET_SKILL_TITLE'
+// export const SET_SKILL_TITLE = 'SET_SKILL_TITLE'
 
-export const ADD_SKILL_DESCRIPTION = 'ADD_SKILL_DESCRIPTION'
+// 添加技能描述
+// export const ADD_SKILL_DESCRIPTION = 'ADD_SKILL_DESCRIPTION'
 
 export const ADD_EXPERIENCE = 'ADD_EXPERIENCE'
 
+// 移除模块（未实现）
 export const REMOVE_PART = 'REMOVE_PART'
 
+// 初始化简历数据
 export const RESTORE_ALL = 'RESTORE_ALL'
 
+// 设置主题颜色
 export const SET_COLOR = 'SET_COLOR'
