5d892b393ed466a2798735ab6a4af302c81edb16
eddievandeer <1255173159@qq.com>
2021-03-04 22:24:43 +0800

    feat(console): add AddButton component

--- src/components/ResumeConsole/Input/AddButton.vue
+++ src/components/ResumeConsole/Input/AddButton.vue
@@ -1,16 +1,59 @@
 <template>
-    <div>
-        <button class="add-button">添加</button>
+    <div class="add-wrapper">
+        <button class="btn normal add-button" @click="handler">
+            <i class="fa fa-plus" aria-hidden="true"></i>
+            <slot></slot>
+        </button>
     </div>
 </template>
 
 <script>
+    import {
+        useStore
+    } from 'vuex'
+
     export default {
-        name: 'AddButton'
+        name: 'AddButton',
+        props: {
+            target: Array,
+            type: {
+                type: String,
+                default: ''
+            },
+            item: String | Object
+        },
+        setup(props) {
+            const store = useStore()
+
+            function handler() {
+                if (props.type.length > 0) {
+                    store.commit(props.type, props.item)
+                } else {
+                    props.target.push(props.item)
+                }
+            }
+
+            return {
+                handler
+            }
+        }
     }
 
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+    .add-wrapper {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+
+        .add-button {
+            border-radius: 25px;
+
+            i {
+                margin-right: .2rem;
+            }
+        }
+    }
 
 </style>
