4d6d8e431c8f1b36989e538a73e53744b5ba18f5
eddievandeer <1255173159@qq.com>
2021-03-24 18:06:20 +0800

    fix(console-AddButton): avoid object reuse

--- src/components/ResumeConsole/Input/AddButton.vue
+++ src/components/ResumeConsole/Input/AddButton.vue
@@ -11,6 +11,7 @@
     import {
         useStore
     } from 'vuex'
+    import copy from '../../../utils/copy'
 
     export default {
         name: 'AddButton',
@@ -29,7 +30,15 @@
                 if (props.type.length > 0) {
                     store.commit(props.type, props.item)
                 } else {
-                    props.target.push(props.item)
+                    if (typeof props.item === 'string') {
+                        props.target.push(props.item)
+                    } else {
+                        let ownItem = {}
+
+                        copy(ownItem, props.item)
+
+                        props.target.push(ownItem)
+                    }
                 }
             }
 
@@ -43,7 +52,6 @@
 
 <style lang="scss" scoped>
     .add-wrapper {
-        width: 100%;
         display: flex;
         justify-content: center;
 
