573eb35bddfb12acd1e59137849520d632221ad7
eddievandeer <1255173159@qq.com>
2021-04-02 20:51:25 +0800

    feat(console): repackage ColorButtons component

--- src/components/ResumeConsole/Input/ColorButtons/index.vue
+++ src/components/ResumeConsole/Input/ColorButtons/index.vue
@@ -1,22 +1,27 @@
 <template>
-    <div class="color-buttons">
+    <div class="color-buttons" v-if="theme.type == 'normal'">
         <button v-for="color in colors" :key="color" :class="['color-button', color]" @click="setColor(color)"></button>
     </div>
 </template>
 
 <script>
     import {
+        watch
+    } from 'vue'
+    import {
         useStore
     } from 'vuex'
     import {
         SET_COLOR
-    } from '../../../store/mutation-types'
-    import colors from '../../../store/colors'
+    } from '../../../../store/mutation-types'
+    import colors from './colors'
 
     export default {
         name: 'ColorButtons',
         setup() {
             const store = useStore()
+
+            const theme = store.state.theme
 
             function setColor(color) {
                 store.commit(SET_COLOR, color)
@@ -24,7 +29,8 @@
 
             return {
                 colors,
-                setColor
+                setColor,
+                theme
             }
         }
     }
