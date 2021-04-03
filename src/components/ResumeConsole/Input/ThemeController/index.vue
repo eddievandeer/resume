e446346a6ab417d269348468bbcfc349f00ac3d7
eddievandeer <1255173159@qq.com>
2021-04-03 14:09:40 +0800

    feat(console): add theme controller to console

---
+++ src/components/ResumeConsole/Input/ThemeController/index.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="theme-controller">
+        <button :class="['btn normal', {'active': storeTheme.type === theme[0]}]" v-for="(theme, key) of themes"
+            :key="key" @click="changeTheme(theme[0])">
+            <span>{{ theme[1] }}</span>
+        </button>
+    </div>
+</template>
+
+<script>
+    import {
+        useStore
+    } from 'vuex'
+
+    import themes from './themes'
+    import {
+        SET_THEME_TYPE
+    } from '../../../../store/mutation-types'
+
+    export default {
+        name: 'ThemeController',
+        setup() {
+            const store = useStore()
+
+            const storeTheme = store.state.theme
+
+            function changeTheme(type) {
+                store.commit(SET_THEME_TYPE, type)
+            }
+
+            return {
+                storeTheme,
+                themes,
+                changeTheme
+            }
+        }
+    }
+
+</script>
+
+<style lang="scss" scoped>
+    .theme-controller {
+        width: 100%;
+        height: 5rem;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+    }
+
+</style>
---
+++ src/components/ResumeConsole/Input/ThemeController/themes.js
@@ -0,0 +1,7 @@
+const themes = new Map()
+
+themes.set('normal', '默认')
+themes.set('artistic', '灰色调')
+themes.set('pure', '白底黑字')
+
+export default themes
