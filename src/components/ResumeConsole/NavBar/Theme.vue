66591f68b184eb303ecdc17fea434433b3775ffe
eddievandeer <1255173159@qq.com>
2021-04-03 14:16:56 +0800

    feat(console): add theme controller to console

--- src/components/ResumeConsole/NavBar/Theme.vue
+++ src/components/ResumeConsole/NavBar/Theme.vue
@@ -1,12 +1,15 @@
 <template>
+    <theme-controller></theme-controller>
     <color-buttons></color-buttons>
 </template>
 
 <script>
+    import ThemeController from '../Input/ThemeController'
     import ColorButtons from '../Input/ColorButtons'
 
     export default {
         components: {
+            ThemeController,
             ColorButtons
         }
     }
