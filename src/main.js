2e9676672fe13a704b22b5ee3bceeb84f804ada9
eddievandeer <1255173159@qq.com>
2021-05-01 22:16:28 +0800

    feat(project): import Loading component

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import store from './store'
 import MyUI from './components/my-ui'
 import MessageBox from './components/my-ui/MessageBox'
+import Loading from './components/my-ui/Loading'
 import './style/global.scss'
 import './theme/default.scss'
 import './theme/artistic.scss'
@@ -13,4 +14,5 @@ const app = createApp(App)
 app.use(store)
 app.use(MyUI)
 app.use(MessageBox)
+app.use(Loading)
 app.mount('#app')
