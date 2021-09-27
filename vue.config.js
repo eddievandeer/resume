aa74dff254151b2e30dfd8ad04bd115be75a51c0
eddievandeer <1255173159@qq.com>
2021-09-27 21:21:29 +0800

    build: use CompressionWebpackPlugin to compress assets

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,30 @@
+const webpack = require('webpack')
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+
 module.exports = {
     publicPath: './',
     productionSourceMap: false,
     devServer: {
         open: true,
         port: 8888
-    }
+    },
+    configureWebpack: {
+        plugins: [
+            new webpack.IgnorePlugin({
+                resourceRegExp: /^\.\/locale$/,
+                contextRegExp: /moment$/
+            }),
+            new CompressionWebpackPlugin({
+                filename: '[path].gz[query]',
+                algorithm: 'gzip',
+                test: /\.(js|css)(\?.*)?$/i,
+                threshold: 10240, // 对超过10k的数据进行压缩
+                minRatio: 0.8, // 只有压缩率小于这个值的资源才会被处理
+                deleteOriginalAssets: false, // 删除原文件
+            }),
+            new webpack.optimize.LimitChunkCountPlugin({
+                maxChunks: 5,
+            })
+        ],
+    },
 }
