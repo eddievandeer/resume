7063bf50f45e6c349928ab08138134a3dfd78729
eddievandeer <1255173159@qq.com>
2022-03-03 18:16:23 +0800

    build: 优化分包策略

--- vue.config.js
+++ vue.config.js
@@ -26,5 +26,27 @@ module.exports = {
                 maxChunks: 5,
             })
         ],
+        optimization: {
+            splitChunks: {
+                chunks: 'all',
+                maxInitialRequests: Infinity,
+                minSize: 10000,
+                cacheGroups: {
+                    vendors: {
+                        name(module) {
+                            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
+
+                            return `npm.${packageName.replace('@', '')}`
+                        },
+                        test: /[\\/]node_modules[\\/]/,
+                        priority: -10,
+                        chunks: 'initial'
+                    }
+                }
+            },
+            runtimeChunk: {
+                name: entrypoint => `runtime-${entrypoint.name}`
+            }
+        }
     },
 }
